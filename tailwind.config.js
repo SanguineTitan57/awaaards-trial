973132f2ac1057c47efb81a7b9e72d9d8181ed22
SanguineTitan57 <69134964+SanguineTitan57@users.noreply.github.com>
2025-06-08 20:43:36 +0300

    Awwwards Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'circular': ['circular-web', 'sans-serif'],
+        'general': ['general', 'sans-serif'],
+        'robert-medium': ['robert-medium', 'sans-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      },
+    },
+  },
+}
